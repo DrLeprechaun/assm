14d54d06b2f99770e5f7fa46950234e1b2789f17
Vadim <leprechaunvanrosenkreuz@gmail.com>
2019-03-29 19:39:44 +0300

    fix base url

--- frontend/src/components/http-common.js
+++ frontend/src/components/http-common.js
@@ -2,14 +2,14 @@ import axios from 'axios'
 import { store } from './../store';
 
 export const AXIOS = axios.create({
-  /*baseURL: `/api`*/
+  baseURL: `/api`
   /*baseURL: `http://localhost:8088/api`,
   headers: {
       'Access-Control-Allow-Origin': 'http://localhost:8080',
       'Access-Control-Allow-Methods': 'POST,GET',
       'Access-Control-Allow-Headers': '*'
     }*/
-  baseURL: `http://localhost:8088/api`,
+  //baseURL: `http://localhost:8088/api`,
   /*headers: {
     'Authorization': 'Bearer ' + ''
   }*/
